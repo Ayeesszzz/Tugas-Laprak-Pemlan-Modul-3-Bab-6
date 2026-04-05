34c49a0629200068aecf450a83628b6f74aad399
Ayeesszzz <farrasibrahim888@gmail.com>
2026-04-05 18:24:38 +0700

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,24 @@
+public class Manager extends Pekerja {
+    private String departemen;
+
+    public Manager(String departemen, double gaji, int tahun, int bulan, int tanggal, int jumlahAnak, String nama, String nik, boolean jenisKelamin, boolean menikah) {
+        super(gaji, tahun, bulan, tanggal, jumlahAnak, nama, nik, jenisKelamin, menikah);
+        this.departemen = departemen;
+    }
+
+    @Override
+    public double getTunjangan() {
+        return super.getTunjangan() + (0.10 * getGaji());
+    }
+
+    @Override
+    public double getPendapatan() {
+        return super.getPendapatan(); 
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n" +
+               "Departemen      : " + departemen;
+    }
+}
---
+++ Manusia.java
@@ -0,0 +1,41 @@
+public class Manusia {
+    private String nama, nik;
+    private boolean jenisKelamin, menikah;
+
+    public Manusia(String nama, String nik, boolean jenisKelamin, boolean menikah) {
+        this.nama = nama;
+        this.nik = nik;
+        this.jenisKelamin = jenisKelamin;
+        this.menikah = menikah;
+    }
+
+    public double getTunjangan() {
+    if (menikah) {
+        if (jenisKelamin) {
+            return 25.0; 
+        } else {
+            return 20.0; 
+        }
+    } else {
+        return 15.0; 
+    }
+}
+
+    public double getPendapatan() { 
+        return getTunjangan();
+    }
+
+    public String toString() {
+    String teks;
+    if (jenisKelamin) {
+        teks = "Laki-laki";
+    } else {
+        teks = "Perempuan";
+    }
+
+    return "Nama            : " + nama + "\n" +
+           "NIK             : " + nik + "\n" +
+           "Jenis kelamin   : " + teks + "\n" +
+           "Pendapatan      : " + getPendapatan();
+    }
+}
